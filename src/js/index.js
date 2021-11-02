cfa242b84a6b05fe5b171a7ad042a79c07016cb2
Atilla Taha Kördüğüm <atillatahaa@gmail.com>
2021-11-02 18:23:17 +0300

    added linter

--- src/js/index.js
+++ src/js/index.js
@@ -1,54 +1,47 @@
-import css from '../style.css';
+import '../style.css';
 
-const taskList = document.querySelector('.list-body');
+const taskListObject = [
+  {
+    description: 'Task 1',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Task 1',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Task w',
+    completed: true,
+    index: 1,
+  },
+];
 
+const createTask = (taskListGet) => {
+  const elementSection = document.querySelector('.list-body');
+  taskListGet.forEach((e) => {
+    const elementUl = document.createElement('ul');
+    const elementLi = document.createElement('li');
+    const elementInput = document.createElement('input');
+    elementInput.name = 'task';
+    elementInput.id = 'task1';
+    elementInput.type = 'checkbox';
+    const elementSpan = document.createElement('span');
+    const elementButton = document.createElement('button');
 
+    elementInput.checked = e.completed;
+    elementLi.appendChild(elementInput);
+    elementSpan.innerHTML = e.description;
+    elementLi.appendChild(elementSpan);
+    elementLi.appendChild(elementButton);
+    elementButton.innerHTML = '<i class="fas fa-window-close"></i>';
 
-const taskListObject =[
-    {
-        description: "Task 1",
-        completed:false,
-        index:1,
-    },
-    {
-        description: "Task 1",
-        completed:false,
-        index:1,
-    },
-    {
-        description: "Task w",
-        completed:true,
-        index:1,
-    }
-]
-window.addEventListener('load',()=>{
-    createTask(taskListObject);
-})
+    elementUl.appendChild(elementLi);
+    elementSection.appendChild(elementUl);
+  });
+};
 
-const createTask= taskListGet =>{
-    const elementSection = document.querySelector('.list-body');
-    taskListGet.forEach((e)=>{
-        const elementUl = document.createElement('ul');
-        const elementLi =document.createElement('li');
-        const elementInput = document.createElement('input');
-        elementInput.name ="task";
-        elementInput.id ="task1";
-        elementInput.type="checkbox";
-        const elementSpan = document.createElement('span');
-        const elementButton = document.createElement('button');
-       
-        
-        elementInput.checked =e.completed;
-        elementLi.appendChild(elementInput);
-        elementSpan.innerHTML =e.description;
-        elementLi.appendChild(elementSpan);
-        elementLi.appendChild(elementButton);
-        elementButton.innerHTML ='<i class="fas fa-window-close"></i>';
-    
-        elementUl.appendChild(elementLi);
-        elementSection.appendChild(elementUl);
-      
-
-     })
-
-}
+window.addEventListener('load', () => {
+  createTask(taskListObject);
+});
